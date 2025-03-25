6e12916347c5a7744004b99558965829af7756f0
maviifreitass <mariavitoria.freitas@yahoo.com>
2025-03-24 23:49:10 -0300

    feat: adding main screen

---
+++ .angular/cache/19.2.1/Frontend_ColaBD/vite/deps/@angular_material_divider.js
@@ -0,0 +1,15 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-WEY7SQZG.js";
+import "./chunk-7IQDSO5B.js";
+import "./chunk-A3AJYSFB.js";
+import "./chunk-4RD3NWAN.js";
+import "./chunk-Z6VC2RM4.js";
+import "./chunk-QUFGTQRT.js";
+import "./chunk-QX2COTFA.js";
+import "./chunk-S35MAB2V.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
