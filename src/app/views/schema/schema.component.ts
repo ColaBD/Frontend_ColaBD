a64136127fe920d7ab237ab2ade0dffc27fda01d
Ruan Sanchez <ruansanchezskt@gmail.com>
2025-04-11 13:11:05 -0300

    adicionando combobox com sgbds de destino

--- src/app/views/schema/schema.component.ts
+++ src/app/views/schema/schema.component.ts
@@ -3,10 +3,12 @@ import { FooterService } from '../../core/footer/services/footer.service';
 import { HighlightModule } from 'ngx-highlightjs';
 import { FormsModule } from '@angular/forms';
 import { CodeEditorComponent } from "./component/code-editor/code-editor.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-schema',
-  imports: [HighlightModule, FormsModule, CodeEditorComponent],
+  imports: [HighlightModule, FormsModule, CodeEditorComponent, MatIconModule, MatButtonModule],
   templateUrl: './schema.component.html',
   styleUrl: './schema.component.scss',
 })
